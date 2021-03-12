deb124174eddf6dbfa324366e9986c5207e8625a
Patryk Makowski <patryk.makowski23@gmail.com>
2021-03-13 00:00:32 +0100

    Fix db initialization

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import cors from 'cors'
-import dotenv from 'dotenv'
 
 import db from "./db/connection"
+import userRouter from "./routes/user-router"
+
+db
 
 const app = express()
 const port = 5000
@@ -14,5 +16,7 @@ app.use(express.json())
 app.get("/", (req, res) => {
   res.send("I'm alive")
 })
+
+app.use("/api/users", userRouter)
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`))
