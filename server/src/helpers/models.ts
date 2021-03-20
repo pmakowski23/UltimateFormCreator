5791a2f4b81fb8d399c72726af7d55982921df91
Patryk Makowski <patryk.makowski23@gmail.com>
2021-03-20 01:59:55 +0100

    Add interfaces for model keys and values

--- server/src/helpers/models.ts
+++ server/src/helpers/models.ts
@@ -1,15 +1,33 @@
-import Form from '../models/forms/form-model'
-import FormField from '../models/forms/formField-model'
-import ReceivedForm from '../models/forms/receivedForm-model'
-import Select from '../models/forms/select-model'
-import User from '../models/users/user-model'
-import Website from '../models/users/website-model'
+import Form, { IForm } from '../models/forms/form-model'
+import FormField, { IFormField } from '../models/forms/formField-model'
+import ReceivedForm, { IReceivedForm } from '../models/forms/receivedForm-model'
+import Select, { ISelect } from '../models/forms/select-model'
+import User, { IUser } from '../models/users/user-model'
+import Website, { IWebsite } from '../models/users/website-model'
 
-export interface IModel {
+export interface IModelTypes {
   form: typeof Form;
   formField: typeof FormField;
   receivedForm: typeof ReceivedForm;
   select: typeof Select;
   user: typeof User;
   website: typeof Website;
+}
+
+export interface IModelKeys {
+  form: keyof IForm;
+  formField: keyof IFormField;
+  receivedForm: keyof IReceivedForm;
+  select: keyof ISelect;
+  user: keyof IUser;
+  website: keyof IWebsite;
+}
+
+export interface IModel {
+  form: IForm;
+  formField: IFormField;
+  receivedForm: IReceivedForm;
+  select: ISelect;
+  user: IUser;
+  website: IWebsite;
 }
