7bac6e8e758c4615c6e39ad5e26b2cf3647c8897
Patryk Makowski <patryk.makowski23@gmail.com>
2021-03-20 16:26:57 +0100

    fix naming and create update endpoint factory

--- server/src/controllers/user-ctrl.ts
+++ server/src/controllers/user-ctrl.ts
@@ -5,6 +5,7 @@ import {
   factoryCreateEndpoint,
   factoryGetAllEndpoint,
   factoryGetOneByIdEndpoint,
+  factoryUpdateEndpoint,
   IAdditionalLogic
 } from '../helpers/ctrl-factory'
 
@@ -26,38 +27,8 @@ export const getUsers = factoryGetAllEndpoint(User)
 // GET /api/users/:id
 export const getUserById = factoryGetOneByIdEndpoint(User)
 
-
 // PUT /api/users/:id
-export const updateUser = async (req: Request, res: Response) => {
-  await User.findOne({ _id: req.params.id }, async (error: Error, userToUpdate: IUser) => {
-    if (error) {
-      return res.status(400).json({ success: false, error })
-    }
-    if (!userToUpdate) {
-      return res.status(404).json({ success: false, error: "User with this Id not found." })
-    }
-
-    const { name, token } = req.body;
-
-    if (name) {
-      userToUpdate.name = name
-    }
-    if (token) {
-      // Check if token is unique
-      // TODO: create this in factory to use validators 
-      // const isUnique = await checkIfUnique(User, "token", token)
-      // if (!isUnique) {
-      //   return res.status(400).json({ success: false, error: "Token is not unique." })
-      // }
-
-      userToUpdate.token = token
-    }
-
-    userToUpdate.save().then(() => {
-      return res.status(200).json({ success: true, data: userToUpdate })
-    }).catch(error => console.log(error))
-  })
-}
+export const updateUser = factoryUpdateEndpoint(User)
 
 // DELETE /api/users/:id
 export const deleteUser = async (req: Request, res: Response) => {
--- server/src/helpers/ctrl-factory.ts
+++ server/src/helpers/ctrl-factory.ts
@@ -1,10 +1,8 @@
 import { Request, Response } from 'express'
-import { IModelTypes, IModel } from './models'
+import { IModelTypes, IModel, IModelKeys, anyOfTypes } from './models'
 import { IValidators, IAdditionalValues } from './validators';
-import mongoose from 'mongoose'
+import mongoose, { CallbackError, QueryOptions } from 'mongoose'
 import capitalize from './capitalize'
-
-// TODO: Add validation option
 
 export type IAdditionalLogic = [
   {
@@ -14,7 +12,7 @@ export type IAdditionalLogic = [
 ]
 
 export const factoryCreateEndpoint =
-  (model: IModelTypes[keyof IModelTypes], additionalLogic?: IAdditionalLogic) =>
+  (model: anyOfTypes<IModelTypes>, additionalLogic?: IAdditionalLogic) =>
     async (req: Request, res: Response) => {
       const body = req.body;
 
@@ -53,19 +51,19 @@ export const factoryCreateEndpoint =
     }
 
 export const factoryGetAllEndpoint =
-  (model: IModelTypes[keyof IModelTypes]) => {
+  (model: anyOfTypes<IModelTypes>) => {
     const modelName = capitalize(model.collection.name)
     return async (_: Request, res: Response) => {
       try {
-        await mongoose.model(modelName).find({}, (error, users) => {
+        await mongoose.model(modelName).find({}, (error, objects) => {
           if (error) {
             return res.status(400).json({ success: false, error })
           }
-          if (!users.length) {
-            return res.status(404).json({ success: false, error: "No users found." })
+          if (!objects.length) {
+            return res.status(404).json({ success: false, error: "No objects found." })
           }
 
-          return res.status(200).json({ success: false, data: users })
+          return res.status(200).json({ success: false, data: objects })
         })
       } catch (error) {
         console.log(error)
@@ -75,18 +73,57 @@ export const factoryGetAllEndpoint =
   }
 
 export const factoryGetOneByIdEndpoint =
-  (model: IModelTypes[keyof IModelTypes]) =>
+  (model: anyOfTypes<IModelTypes>) =>
     async (req: Request, res: Response) => {
-      await model.findOne({ _id: req.params.id }, (error: Error, user: IModel[keyof IModel]) => {
+      await model.findOne({ _id: req.params.id }, (error: Error, object: IModel[keyof IModel]) => {
         if (error) {
           return res.status(400).json({ success: false, error })
         }
-        if (!user) {
+        if (!object) {
           return res.status(404).json({ success: false, error: "User with this Id not found." })
         }
 
-        return res.status(200).json({ success: true, data: user })
+        return res.status(200).json({ success: true, data: object })
       }).catch(error => console.log(error))
+    }
+
+export const factoryUpdateEndpoint =
+  (model: anyOfTypes<IModelTypes>, additionalLogic?: IAdditionalLogic) =>
+    async (req: Request, res: Response) => {
+      await model.findOne({ _id: req.params.id }, async (error: Error, objectToUpdate: any) => {
+        if (error) {
+          return res.status(400).json({ success: false, error })
+        }
+        if (!objectToUpdate) {
+          return res.status(404).json({ success: false, error: "User with this Id not found." })
+        }
+
+        if (additionalLogic) {
+          try {
+            for (const element in additionalLogic) {
+              const { validator, additionalVariables } = additionalLogic[element]
+              await validator(additionalVariables, req.body)
+            }
+          } catch (error) {
+            console.log(error)
+            return res.status(400).json({ success: false, error })
+          } finally {
+            console.log("All validation succeeded")
+          }
+        }
+
+        const body = <IModel[keyof IModel]>req.body
+        for (let [key, value] of Object.entries(body)) {
+          objectToUpdate[key] = value
+        }
+
+        try {
+          objectToUpdate.save()
+          return res.status(200).json({ success: true, data: objectToUpdate })
+        } catch (error) {
+          return res.status(400).json({ success: false, error })
+        }
+      })
     }
 
 // TODO: create rest methods.
