87d477b0a13f9e627f3f323036afe03840897565
Patryk Makowski <patryk.makowski23@gmail.com>
2021-03-22 15:38:39 +0100

    Fix FormField interface

--- server/src/models/forms/formField-model.ts
+++ server/src/models/forms/formField-model.ts
@@ -3,15 +3,16 @@ import { ISelect, SelectSchema } from './select-model'
 
 export interface FormFieldData {
   label: string;
-  field_type: string;
+  field_type: "text" | "select" | "checkbox" | "radio" | "textarea" |
+  "password" | "email" | "number" | "date" | "time" | "button" | "phone";
   isRequired: boolean;
-  placeholder: string;
-  target: string;
-  isDisabled: boolean;
-  minValue: string;
-  maxValue: string;
-  Select: ISelect;
-  pattern: string;
+  placeholder?: string;
+  target?: "_blank" | "_self" | "_parent" | "_top";
+  isDisabled?: boolean;
+  minValue?: string;
+  maxValue?: string;
+  Select?: ISelect;
+  pattern?: string;
 }
 
 export interface IFormField extends FormFieldData, Document {
@@ -44,19 +45,13 @@ export const FormFieldSchema: Schema = new Schema({
   target: { type: String, enum: ["_blank", "_self", "_parent", "_top"], required: false },
   isDisabled: { type: Boolean, required: false },
   minValue: { type: Number, required: false },
-  maxValue: {
-    type: Number, required: false, validation: {
-      validator: function (this: IFormField) {
-        return false // TODO: It's not doign anything
-      }
-    }
-  },
+  maxValue: { type: Number, required: false, },
   Select: {
     type: SelectSchema, required: function (this: IFormField) {
       return this.field_type === ("select" || "checkbox" || "radio");
     }
   },
-  pattern: { type: String, required: false } // TODO: must be valid regex
+  pattern: { type: String, required: false } // TODO: must be valid regex 
 })
 
 export default mongoose.model<ISelect>("Formfields", FormFieldSchema)
