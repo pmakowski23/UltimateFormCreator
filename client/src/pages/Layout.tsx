84f7bda95844557e5b03818454e65361b40a3946
Dorian Opacki <dorianwew@gmail.com>
2021-03-26 19:27:35 +0100

    add layout

---
+++ client/src/pages/Layout.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from 'react';
+
+export const Layout: FunctionComponent = () => {
+  return (
+    <div className="bg-theme-main h-screen p-10">
+      <div className="bg-theme-secondary h-full"></div>
+    </div>
+  );
+};
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      theme: {
+        main: '#424250',
+        secondary: '#33333D',
+      },
+    },
     extend: {},
   },
   variants: {
