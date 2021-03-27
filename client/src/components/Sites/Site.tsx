ff6419598ee2acbb4b2367ebdb78b232def23710
Patryk Makowski <patryk.makowski23@gmail.com>
2021-03-27 23:29:47 +0100

    Change Site Styling to emotion from tailwind

--- client/src/components/Sites/Site.tsx
+++ client/src/components/Sites/Site.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/css';
 import { FC } from 'react';
 import { Paragraph } from '../styles/Paragraph';
 
@@ -5,16 +6,28 @@ const requestImageFile = require.context('../../images', true, /^\.\/.*\.svg$/);
 
 interface ISite {
   img: string;
-  siteName: string;
+  siteName?: string;
 }
 
 export const Site: FC<ISite> = ({ img, siteName }) => {
   const imgUrl = `./${img}.svg`;
 
   return (
-    <div className="flex flex-col items-center justify-center h-48 w-48 bg-theme-button rounded-xl shadow-lg">
-      <img className="h-36 w-36" src={requestImageFile(imgUrl).default} alt={img}></img>
-      <Paragraph>{siteName}</Paragraph>
+    <div
+      className={css({
+        backgroundColor: '#2B2B34',
+        height: '240px',
+        width: '240px',
+        borderRadius: '30px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+      })}
+    >
+      <img className={css({ height: '170px' })} src={requestImageFile(imgUrl).default} alt={img}></img>
+      <Paragraph>{siteName ? siteName : ''}</Paragraph>
     </div>
   );
 };
