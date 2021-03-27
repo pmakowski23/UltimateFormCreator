f5634c16ddb4ab8e36fedebd83f666c970785e4b
Patryk Makowski <patryk.makowski23@gmail.com>
2021-03-27 21:16:55 +0100

    Use new Heading Component

--- client/src/pages/Sites.tsx
+++ client/src/pages/Sites.tsx
@@ -1,11 +1,12 @@
 import { FC } from 'react';
 import { Layout } from '../components/shared/Layout';
 import { Site } from '../components/Sites/Site';
+import { Heading } from '../components/styles/Heading';
 
 export const Sites: FC = () => {
   return (
     <Layout>
-      <p className="font-sans text-white">Your Sites</p>
+      <Heading>Your Sites</Heading>
       <Site img="atom" siteName="My new Site" />
     </Layout>
   );
