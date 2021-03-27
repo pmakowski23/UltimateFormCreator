f78d159bc77e9fe97b0e6a82354b6555bc38a63b
Patryk Makowski <patryk.makowski23@gmail.com>
2021-03-27 21:33:34 +0100

    Create Logo component

---
+++ client/src/components/shared/Logo.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react';
+import { Heading } from '../styles/Heading';
+import logo from '../../images/logo.svg';
+import { css } from '@emotion/css';
+
+export const Logo: FC = () => {
+  return (
+    <div
+      className={css({
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '300px',
+        textAlign: 'center',
+      })}
+    >
+      <img className={css({ height: '200px' })} src={logo} alt="logo" />
+      <Heading>Ultimate Form Creator</Heading>
+    </div>
+  );
+};
